3833d055b224c642115eb3fb0326fa1d50c2f576
henryupton <henry.upton@envato.com>
2025-07-29 14:51:28 +1200

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="terraformat",
-    version="0.1.4",
+    version="0.1.5",
     author="Henry Upton-Birdsall",
     author_email="henryupton@gmail.com",
     description="A wrapper for the Terraform CLI that provides a formatted plan summary.",
